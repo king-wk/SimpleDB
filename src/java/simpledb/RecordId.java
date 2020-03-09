55ec0b70d60b31232cf488462577201b4eec6e74
wenjingjing <1811507@mail.nankai.edu.cn>
2020-03-09 13:18:08 +0800

    RecordId modification.

--- src/java/simpledb/RecordId.java
+++ src/java/simpledb/RecordId.java
@@ -13,11 +13,9 @@ public class RecordId implements Serializable {
     /**
      * Creates a new RecordId referring to the specified PageId and tuple
      * number.
-     * 
-     * @param pid
-     *            the pageid of the page on which the tuple resides
-     * @param tupleno
-     *            the tuple number within the page.
+     *
+     * @param pid     the pageid of the page on which the tuple resides
+     * @param tupleno the tuple number within the page.
      */
     public RecordId(PageId pid, int tupleno) {
         // some code goes here
@@ -42,19 +40,25 @@ public class RecordId implements Serializable {
     /**
      * Two RecordId objects are considered equal if they represent the same
      * tuple.
-     * 
+     *
      * @return True if this and o represent the same tuple
      */
     @Override
     public boolean equals(Object o) {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        if (o == this) {
+            return true;
+        } else if (o instanceof RecordId) {
+            RecordId another = (RecordId) o;
+            return (another.getPageId().equals(this.getPageId()) && another.getTupleNumber() == this.getTupleNumber());
+        } else return false;
+        //throw new UnsupportedOperationException("Implement this");
     }
 
     /**
      * You should implement the hashCode() so that two equal RecordId instances
      * (with respect to equals()) have the same hashCode().
-     * 
+     *
      * @return An int that is the same for equal RecordId objects.
      */
     @Override
